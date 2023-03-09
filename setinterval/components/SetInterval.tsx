4d45f4e45c772ce3453b98e69609c7b6ac67710d
brgndy <brgndy96@gmail.com>
2023-03-09 17:08:01 +0900

    2s

--- setinterval/components/SetInterval.tsx
+++ setinterval/components/SetInterval.tsx
@@ -5,27 +5,52 @@ import { useEffect, useRef, useState } from "react";
 export default function SetInterval() {
   const inputRef = useRef<HTMLInputElement>(null);
   const [number, setNumber] = useState(60);
+  let count = 1;
+  let intervalRef = useRef<NodeJS.Timeout | null>(null);
 
   const inputBlurHandler = () => {
     if (inputRef.current) {
       let num = Number(inputRef.current.value);
       num = num === 0 ? 1 : num;
-      console.log("현재 숫자 : ", num);
+
       setNumber(num);
+      console.log("현재 숫자 : ", number);
       inputRef.current.value = num.toString();
+
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+      intervalRef.current = setInterval(() => {
+        console.log(count);
+        count++;
+        if (count === 5) {
+          count = 1;
+        }
+      }, (60 / num) * 1000);
     }
   };
 
   const numberOnChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     let num = Number(e.target.value);
 
+    // 최소 숫자 1과 최대숫자 200으로 지정
     if (inputRef.current && num < 1) {
       num = 1;
       inputRef.current.value = num.toString();
+    } else if (inputRef.current && num > 200) {
+      num = 200;
+      inputRef.current.value = num.toString();
     }
-
     setNumber(num);
   };
+
+  useEffect(() => {
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, []);
 
   return (
     <>
